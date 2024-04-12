d3c1d118a8d15654d6934faa8a9c49784d02e6b1
૮ ˶ᵔ ᵕ ᵔ˶ ა <101676768+redbloom@users.noreply.github.com>
2024-04-11 19:20:58 -0600

    #

--- app/src/main/java/com/example/chanchitodigital/navigation/AppScreens.kt
+++ app/src/main/java/com/example/chanchitodigital/navigation/AppScreens.kt
@@ -3,4 +3,6 @@ package com.example.chanchitodigital.navigation
 sealed class AppScreens (val route: String) {
     object SplashScreen: AppScreens("splash_screen")
     object MainScreen: AppScreens("main_screen")
+    object LoginScreen: AppScreens("login_screen")
+
 }
