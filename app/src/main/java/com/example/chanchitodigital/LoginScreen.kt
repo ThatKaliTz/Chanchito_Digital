82134e2cd5d3836f8c50066d234d68029a9a1cf5
૮ ˶ᵔ ᵕ ᵔ˶ ა <101676768+redbloom@users.noreply.github.com>
2024-04-11 21:59:49 -0600

    Diseño

--- app/src/main/java/com/example/chanchitodigital/LoginScreen.kt
+++ app/src/main/java/com/example/chanchitodigital/LoginScreen.kt
@@ -1,21 +1,133 @@
 package com.example.chanchitodigital
 
 import android.content.res.Configuration
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.chanchitodigital.ui.theme.Black
+import com.example.chanchitodigital.ui.theme.ElectricViolet
+import com.example.chanchitodigital.ui.theme.HanPurple
+import com.example.chanchitodigital.ui.theme.Indigo
+import com.example.chanchitodigital.ui.theme.LightGrey
+import com.example.chanchitodigital.ui.theme.Orchid
+import com.example.chanchitodigital.ui.theme.VividViolet
 
 @Composable
-fun LoginScreen(navController: NavController) {
-    Column (modifier = Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center) {
-        Text("Login Screen")
+fun LoginScreen() {
+    val gradientColors = listOf(
+        HanPurple,
+        Indigo,
+        ElectricViolet,
+        VividViolet
+    )
+
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        // Fondo de la pantalla
+        Background(gradientColors = gradientColors)
+        // Contenido de la pantalla
+        Content()
     }
+}
+
+@Composable
+fun Background(gradientColors: List<Color>) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    brush = GradientBackgroundBrush(
+                        isVerticalGradient = false,
+                        colors = gradientColors
+                    ),
+                )
+        )
+    }
+}
+
+@Composable
+fun Content(modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(top = 300.dp),
+        shape = RoundedCornerShape(topEndPercent = 30)
+    ) {
+        // Contenido
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(10.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TitleAndInfo()
+        }
+
+    }
+}
+
+@Composable
+fun TitleAndInfo() {
+    // Titulo
+    MyText(
+        "Bienvenido a Chanchito Digital",
+        Black,
+        MaterialTheme.typography.titleLarge)
+    // Instrucciones
+    MyText(
+        "Seleccione una opción para comenzar.",
+        LightGrey,
+        MaterialTheme.typography.bodySmall)
+}
+@Composable
+fun MyText(text: String, color: Color, style: TextStyle) {
+    Text(text, color = color , style = style)
+}
+
+@Composable
+fun GradientBackgroundBrush(
+    isVerticalGradient: Boolean,
+    colors: List<Color> ): Brush {
+    val endOffset = if (isVerticalGradient) {
+        Offset(0.0f, Float.POSITIVE_INFINITY)
+    } else {
+        Offset(Float.POSITIVE_INFINITY, 0.0f)
+    }
+
+    return Brush.linearGradient(
+        colors = colors,
+        start = Offset.Zero,
+        end = endOffset
+    )
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun ContentPreview() {
+    LoginScreen()
 }
--- app/src/main/java/com/example/chanchitodigital/ui/theme/Color.kt
+++ app/src/main/java/com/example/chanchitodigital/ui/theme/Color.kt
@@ -6,6 +6,14 @@ val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
 val White = Color.White
+val LightGrey = Color(0xFF8F8E8E)
+
+// Gradient
+val HanPurple = Color(0xFF5E17EB)
+val Indigo = Color(0xFF7612EF)
+val ElectricViolet = Color(0xFF870EF2)
+val VividViolet = Color(0xFF9b0af6)
+val Orchid = Color(0xFFCC00FF)
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
